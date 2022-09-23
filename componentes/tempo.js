204e8d0fc513d9da7fee282b5ce02d26181e5f8c
lorenasobral <79669642+lorenasobral@users.noreply.github.com>
2022-09-23 14:43:05 -0300

    update tempo

--- componentes/tempo.js
+++ componentes/tempo.js
@@ -7,7 +7,31 @@ export default function components(props) {
    <>
         <Text style={styles.titulo}>Dados do Tempo</Text>
         <Text style={styles.texto}>Cidade: {data.city_name}</Text>
+        <Text style={styles.texto}>Descrição: {data.description}</Text>import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+export default function components(props) {
+    let data = props.data;
+ return (
+   <>
+        <Text style={styles.titulo}>Dados do Tempo</Text>
+        <Text style={styles.texto}>Cidade: {data.city_name}</Text>
         <Text style={styles.texto}>Descrição: {data.description}</Text>
+        <Text style={styles.texto}>Temperatura: {data.temp}°C</Text>
+
+   </>
+  );
+}
+
+const styles = StyleSheet.create({
+    titulo:{
+        fontSize:25
+    },
+    texto:{
+        fontSize:20
+    }
+})
+
         <Text style={styles.texto}>Temperatura: {data.temp}°C</Text>
 
    </>
